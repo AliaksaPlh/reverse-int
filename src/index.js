51d819e1ea5b28d54e575f5ca373c6f0a16dee9f
AliaksaPlh <sashvero@gmail.com>
2023-08-28 17:29:55 +0300

    feat: complete tasl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+let i = 0;
+let str = `${Math.abs(n)}`;
+let result = ''
+while (i < str.length) {
+    result = `${str[i]}${result}`;
+    i +=1;
+};
+return +result;
+
+
+
 }
