af164a9733cbafe7674f3a6460ab3f54773bb0bf
AliaksaPlh <sashvero@gmail.com>
2023-08-28 17:30:36 +0300

    feat: add array method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 module.exports = function reverse (n) {
-let i = 0;
-let str = `${Math.abs(n)}`;
-let result = ''
-while (i < str.length) {
-    result = `${str[i]}${result}`;
-    i +=1;
-};
-return +result;
+// let i = 0;
+// let str = `${Math.abs(n)}`;
+// let result = ''
+// while (i < str.length) {
+//     result = `${str[i]}${result}`;
+//     i +=1;
+// };
+// return +result;
 
-
+    const str = `${Math.abs(n)}`;
+    const arrStr = Array.from(str); 
+    let result = arrStr.reverse();
+    return +result.join('');
 
 }
